76492c2b59dd822728e9fa8f16c236da560c9af2
moosoul <m.moosoul@gmail.com>
2016-04-19 16:18:05 +0800

    删除MSSliderView

--- MSBaseCustomView/MSBaseCustomView/ViewController.m
+++ MSBaseCustomView/MSBaseCustomView/ViewController.m
@@ -7,14 +7,11 @@
 //
 
 #import "ViewController.h"
-#import "MSSliderView.h"
 
-
-#import "CustomView.h"
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet MSSliderView *sliderView;
+
 
 @end
 
@@ -22,34 +19,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-//    CustomView *customView1 = [[CustomView alloc] initWithFrame:CGRectMake(0, 0, 320, 100)];
-//    [self.view addSubview:customView1];
-//    
-//    
-//    NSLog(@"--------------------------------\n--------------------------------");
-//    
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        CustomView *customView2 = [[CustomView alloc] init];
-//        [self.view addSubview:customView2];
-//        customView2.frame = CGRectMake(0, 200, 320, 100);
-//    });
-    
-
-    
-    
-//    MSSliderView *sliderView = [[MSSliderView alloc] initWithFrame:CGRectMake(0, 300, 320, 64)];
-//    sliderView.fontSize = 15.0f;
-//    sliderView.selectedFontSizeBold = YES;
-//    sliderView.defaultSelectedIndex = 0;
-//    sliderView.scrollAnimated = YES;
-//    sliderView.showUnderline = YES;
-//    sliderView.underlineHeight = 3.0f;
-//    sliderView.datasource = [NSMutableArray arrayWithArray:@[@"AAAAAAAAAA", @"VCDADSDASD", @"AA", @"CCCCS"]];
-//
-//    [self.view addSubview:sliderView];
-
-    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -58,22 +27,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)setSliderView:(MSSliderView *)sliderView
-{
-    sliderView.fontSize = 14.0f;
-    sliderView.selectedFontSizeBold = YES;
-    sliderView.defaultSelectedIndex = 0;
-    sliderView.scrollAnimated = YES;
-    sliderView.showUnderline = YES;
-    sliderView.underlineHeight = 3.0f;
-    sliderView.autoWidth = YES;
-    sliderView.margin = 32.0f;
-    
-    
-    sliderView.datasource = [NSMutableArray arrayWithArray:@[@"AAAAAAAAAA", @"VCDADSDASD", @"AA", @"CCCCS", @"CCCCC", @"CCCCC", @"CCCCC", @"CCCCC"]];
-    
-    _sliderView = sliderView;
-}
+
 
 
 
